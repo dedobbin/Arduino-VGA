f5bc9a5142ee15321bc15799acde9d111a80ec69
debobbin <you@example.com>
2021-01-08 22:16:07 +0100

    setup some nice patterns

--- automaton.h
+++
@@ -1,5 +0,0 @@
-struct automaton{
-  byte x;
-  byte y;
-  byte type;
-};
--- ram.ino
+++ ram.ino
@@ -1,33 +1,40 @@
-//#define FILL_RAM
+#define FILL_RAM
 
 #include "sram.h"
 #ifndef FILL_RAM
-#include "automaton.h"
 #include <VGAX.h>
 #endif
+
+int ram_addr = 0;
 
 #ifndef FILL_RAM
 VGAX vga;
 #endif
 
-int n_entries = 30;
-int addr = 0;
-
 #ifdef FILL_RAM
+int n_entries = 30;
+
 void fill_ram()
 {
-  initRam();
-
-  byte data[n_entries * 3]; 
+  const int VGA_BWIDTH = 30;
+  const int VGA_HEIGHT = 60;
+  const int VGA_WIDTH = VGA_BWIDTH*4;
+  byte vga_data[VGA_HEIGHT*VGA_BWIDTH];
+  memset(vga_data, 0, sizeof(vga_data));
   
-  int y = 0;
-  for (int i = 0; i < n_entries * 3; i+=3){
-    data[i] = 10;
-    data[i + 1] = y++;
-    data[i + 2] = 1;
+  for (int i=0;i<n_entries;i++){
+    byte x = rand()%30 *4;
+    byte y = rand()%VGA_HEIGHT;
+    byte clr = rand()%3+1;
+    
+    //vga.putpixel(automatons[i].x, automatons[i].y, automatons[i].type);
+    byte *p=vga_data + y*VGA_BWIDTH + (x>>2);
+    byte bitpos=6-(x & 3)*2;
+    *p=(*p & ~(3 <<bitpos)) | clr <<bitpos;
   }
-
-  writeRam((char*)pixel_data, addr, n_entries * 3);
+  
+  initRam();
+  writeRam(vga_data, ram_addr, sizeof(vga_data));
 }
 #endif
 
@@ -35,45 +42,55 @@ void setup()
 {  
 #ifndef FILL_RAM
   vga.begin();
-  
-  automaton automatons[n_entries];
-
-  automaton* a_ptr = automatons;
-  for (int i=0;i<n_entries;i++){
-    automaton a;
-    a.x = rand()%VGAX_WIDTH;
-    a.y = rand()%VGAX_HEIGHT;
-    a.type =  (i%3)+1;
-    *a_ptr = a;
-    //*a_ptr = {rand()%VGAX_WIDTH, rand()%VGAX_HEIGHT, (i%3)+1, i*sizeof(automaton)};
-    a_ptr++;
-  }
-
-  byte* vga_ptr = vgaxfb;
-  for (int i=0;i<n_entries;i++){
-    //vga.putpixel(automatons[i].x, automatons[i].y, automatons[i].type);
-    byte *p=vga_ptr +  automatons[i].y*VGAX_BWIDTH + (automatons[i].x>>2);
-    byte bitpos=6-(automatons[i].x & 3)*2;
-    *p=(*p & ~(3 <<bitpos)) | automatons[i].type <<bitpos;
-  }
-
-   
-
-//  initRam();
-//  int buffer_size = n_pix * 3;
-//  char buffer [buffer_size];
-//  readRam(buffer, addr, buffer_size);
-//
-//  for (int i =0; i < buffer_size; i+=3){
-//      vga.putpixel(buffer[i], buffer[i+1], buffer[i+2]);
-//  }
-  
+  initRam();
+  readRam(vgaxfb, ram_addr, VGAX_HEIGHT*VGAX_BWIDTH);
 #else
   fill_ram();
 #endif    
 }
 
 void loop()                    
-{
-    
+{   
+#ifndef FILL_RAM
+  for(;;){
+    byte x = rand()%VGAX_WIDTH;
+    byte y = rand()%VGAX_HEIGHT;
+    byte pix = vga.getpixel(x, y);
+    //TODO: pick pixel based on audio input
+    if (pix!=0){
+      if (x-1 >= 0){ 
+        vga.putpixel(x-1, y, pix);
+      }
+      if (x+1 < VGAX_WIDTH){
+        vga.putpixel(x+1, y, pix);
+      }
+      if (y-1 >= 0){
+        vga.putpixel(x, y-1, pix);
+      }
+      if (y+1 < VGAX_HEIGHT){
+        vga.putpixel(x, y+1, pix);
+      }
+      //TODO: flip on/off based on audio input
+      if (rand()%10==0){
+        vga.putpixel(x,y,rand()%3+1);
+      }
+    }
+  }
+
+//  for (int y=0;y<VGAX_HEIGHT;y++){
+//    for (int x=0;x<VGAX_WIDTH;x++){
+//      byte pix = vga.getpixel(x, y);
+//      switch(pix){
+//        case 0: //black
+//          break;
+//        case 1: //red
+//          break;
+//        case 2: //green
+//          break;
+//        case 3: // yellow
+//          break;
+//      }
+//    }
+//  }
+#endif
 }
